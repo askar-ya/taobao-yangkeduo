f860a686e15e8830e15e2e7b6d5c89c940f03b03
Askar Yaparov <52990620+askar-ya@users.noreply.github.com>
2024-06-09 13:28:25 +0500

    fix

--- logic.py
+++ logic.py
@@ -53,7 +53,7 @@ def pars_yangkeduo(link: str):
 
 def pars_taobao(link: str):
     with sync_playwright() as playwright:
-        browser = playwright.firefox.launch(headless=False)
+        browser = playwright.firefox.launch(headless=True)
         context = browser.new_context()
         with open('taobao_cookies.json', 'r', encoding='utf-8') as f:
             context.add_cookies(json.load(f))
