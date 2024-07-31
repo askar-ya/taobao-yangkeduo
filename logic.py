41a2663ea0f7a0b064bc231128da66a4bd4e60f7
askar-ya <askaryaparov@yandex.ru>
2024-07-31 16:09:42 +0500

    fix

--- logic.py
+++ logic.py
@@ -53,7 +53,7 @@ def pars_yangkeduo(link: str):
 
 
 def pars_taobao(link: str):
-    with sync_playwright() as playwright:
+    with (sync_playwright() as playwright):
         browser = playwright.chromium.launch(headless=True)
         context = browser.new_context()
         with open('taobao_cookies.json', 'r', encoding='utf-8') as f:
@@ -87,26 +87,33 @@ def pars_taobao(link: str):
                 print(e)
                 out = {'ok': False}
         elif link[:12] == 'https://item':
-            out = {'ok': True}
+            try:
+                out = {'ok': True}
 
-            page.wait_for_selector('div[class="ItemTitle--root--3V3R5Y_"]')
-            title = page.query_selector('div[class="ItemTitle--root--3V3R5Y_"]').inner_text()
+                page.wait_for_selector('div[class="ItemTitle--root--3V3R5Y_"]')
+                title = page.query_selector('div[class="ItemTitle--root--3V3R5Y_"]').inner_text()
 
-            out['title'] = translator.translate(title, dest='ru').text
-            price = page.query_selector('span[class="Price--priceText--1oEHppn"]').inner_text()
+                out['title'] = translator.translate(title, dest='ru').text
+                price = page.query_selector('span[class="Price--priceText--1oEHppn"]').inner_text()
+                out['price'] = f'{price} ¥'
+                img_box = page.query_selector('ul[class="PicGallery--thumbnails--3EG14Q2"]').query_selector_all('img')
+                out['img'] = []
+                for i in img_box:
+                    url = i.get_attribute('src').split('.jpg')[0]
+                    if url[-4:] != 'webp':
+                        url += '.jpg'
+                    out['img'].append('https:' + url)
+                variations = page.query_selector_all('div[class="SkuContent--content--2UKSo-9"]')
+                specifications = ''
+                if variations:
+                    for sku in variations:
+                        specifications += translator.translate(sku.inner_text(), dest='ru').text
 
-            out['price'] = f'{price} ¥'
-            img_box = page.query_selector('ul[class="PicGallery--thumbnails--3EG14Q2"]').query_selector_all('img')
-            out['img'] = []
-            for i in img_box:
-                out['img'].append('https:' + (i.get_attribute('src').split('.jpg')[0] + '.jpg'))
-            variations = page.query_selector_all('div[class="SkuContent--content--2UKSo-9"]')
-            specifications = ''
-            if variations:
-                for sku in variations:
-                    specifications += translator.translate(sku.inner_text(), dest='ru').text
+                out['specifications'] = specifications
 
-            out['specifications'] = specifications
-
-            out = {'ok': False}
+            except Exception as e:
+                print(e)
+                out = {'ok': False}
+            print(out)
         return out
+
